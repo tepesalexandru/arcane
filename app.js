5fff791e7f00aceadf5cab71f89cbf558844e413
LupusDiamond <godzplayer@hotmail.com>
2020-01-23 21:39:37 +0200

    some more game logic

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/// Game Object
+
+let gameConfig = {
+  wave: 0,
+  round: 0
+};
+
 /// Player Object
 
 let player = {
@@ -5,7 +12,8 @@ let player = {
   energy: 0,
   shield: 0,
   attack: 10,
-  gold: 0
+  gold: 0,
+  alive: false
 };
 
 /// Monster
@@ -21,7 +29,8 @@ new Vue({
   el: "#app",
   data: {
     user: player,
-    enemy: monster
+    enemy: monster,
+    game: gameConfig
   },
   computed: {
     enemyHealth: function() {
@@ -29,16 +38,37 @@ new Vue({
     }
   },
   watch: {
-    enemy: function(value) {}
+    enemy: {
+      handler(val) {
+        if (this.enemy.health <= 0) this.enemy.alive = false;
+        else this.enemy.alive = true;
+      },
+      deep: true
+    }
   },
   methods: {
     rstGame: function() {
+      /*player.health = 100;
+      monster.health = 80;
+      monster.alive = true;*/
+    },
+    rstPlayer: function() {
       player.health = 100;
+    },
+    rstEnemey: function() {
       monster.health = 80;
       monster.alive = true;
     },
     playerAttack: function(amount) {
       monster.health -= amount;
+    },
+    loadNextRound: function(curRound) {
+      gameConfig.round++;
+      if (gameConfig.round >= 3) {
+        this.rstEnemey();
+        gameConfig.round = 0;
+        gameConfig.wave++;
+      }
     }
   }
 });
