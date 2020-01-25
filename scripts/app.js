06701afec95f27b1c972b020d714e1f34fac211c
LupusDiamond <godzplayer@hotmail.com>
2020-01-25 17:11:40 +0200

    basic round functionality

--- scripts/app.js
+++ scripts/app.js
@@ -32,9 +32,19 @@ new Vue({
         if (this.enemy.health <= 0) {
           this.enemy.alive = false;
           this.rstEnemey();
+          this.endRound(true);
           this.loadNextRound();
           this.user.gold += this.enemy.reward;
         } else this.enemy.alive = true;
+      },
+      deep: true
+    },
+    game: {
+      handler(val) {
+        if (this.game.playerLog[0]) __playerRounds[0].classList += wonClass;
+        else __playerRounds[0].classList.add(lostClass);
+        console.log("hi");
+        console.log(__playerRounds[0]);
       },
       deep: true
     }
@@ -55,20 +65,33 @@ new Vue({
       this.game = { ...initialConfig };
     },
     playerAttack: function(amount) {
-      // Check if the player is alive
       if (this.user.alive == true) {
         this.enemy.health -= amount;
       }
     },
     enemyAttack: function(amount) {
       this.user.health -= amount;
-      console.log("Player hit!");
     },
     loadNextRound: function(curRound) {
       this.game.round++;
       if (this.game.round >= 3) {
         this.game.round = 0;
         this.game.wave++;
+      }
+    },
+    endRound: function(playerWon) {
+      let playerWins = this.game.playerLog[0] + this.game.playerLog[1];
+      let enemyWins = this.game.enemyLog[0] + this.game.enemyLog[1];
+      if (playerWon) this.game.playerLog[playerWins] = true;
+      else this.game.enemyLog[enemyWins] = true;
+    },
+    barColor: function(round, target) {
+      if (target == 1) {
+        if (this.game.playerLog[round]) return wonClass;
+        else return lostClass;
+      } else {
+        if (this.game.enemyLog[round]) return wonClass;
+        else return lostClass;
       }
     }
   }
--- scripts/config.js
+++ scripts/config.js
@@ -2,10 +2,26 @@
 
 let gameConfig = {
   wave: 0,
-  round: 0
+  round: 0,
+  playerLog: [0, 0],
+  enemyLog: [0, 0]
 };
 
 const initialConfig = {
   wave: 0,
-  round: 0
+  round: 0,
+  playerLog: [0, 0],
+  enemyLog: [0, 0]
 };
+
+/// DOM Elements
+
+const __playerRounds = document.querySelectorAll(".rnd-player");
+const __enemyRounds = document.querySelectorAll(".rnd-enemy");
+const wonClass = "bg-green-500";
+const lostClass = "bg-red-500";
+
+function updateRounds() {
+  /*if (gameConfig.roundsWon == 1) gameRounds[0].classList += wonClass;
+  if (gameConfig.roundsWon == 2) gameRounds[1].classList += wonClass;*/
+}
